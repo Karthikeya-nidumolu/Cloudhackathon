fcac5ebd38d0467f25b8c99a2c336b3a81243006
NIDUMOLU KARTHIKEYA <2320030065@klh.edu.in>
2026-03-26 21:39:44 +0530

    Update firebase.ts

--- lib/firebase.ts
+++ lib/firebase.ts
@@ -5,10 +5,10 @@ import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCeAjUe-Fun3iaebNzAPlqMF0nJFXHv03M",
-  authDomain: "e-learning-625e1.firebaseapp.com",
-  projectId: "e-learning-625e1",
-  appId: "1:900604154673:web:4d59984f1d7d45262a9610",
+  apiKey: "yourapi",
+  authDomain: ",
+  projectId: "",
+  appId: "",
 };
 
 const app = initializeApp(firebaseConfig);
